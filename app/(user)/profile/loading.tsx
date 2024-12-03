fdad2e45452a9b3f0ff1241080cafdb826763199
MARUNGs <mi1764@naver.com>
2024-12-03 10:01:31 +0900

    로딩 수정

--- app/(user)/profile/loading.tsx
+++ app/(user)/profile/loading.tsx
@@ -1,15 +1,13 @@
 export const dynamic = "force-dynamic";
 
-async function wait() {
-  return new Promise((resolve) => setTimeout(resolve, 100000));
-}
-
 export default async function ProfileLoading() {
-  // await wait();
-
   return (
-    <div className="size-40 bg-stone-500">
-      <div className="size-full bg-stone-500"></div>
+    <div className="flex flex-col items-center">
+      <div>
+        <div className="w-32 h-10 bg-stone-500 rounded-lg my-5" />
+        <div className="w-40 h-10 bg-stone-500 rounded-lg" />
+      </div>
+      <div className="flex flex-col items-center my-10 w-3/5 h-60 border rounded-md"></div>
     </div>
   );
 }
