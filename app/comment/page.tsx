51c626750b35a190f4e8261145b0cc3017155f05
MARUNGs <mi1764@naver.com>
2024-12-10 13:11:09 +0900

    CommentList Type 정의

--- app/comment/page.tsx
+++ app/comment/page.tsx
@@ -3,8 +3,9 @@ import { formatToTimeAgo } from "../lib/format";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { findComments } from "./server";
+import { CommentListProps } from "../types/CommentListType";
 
-export default async function CommentList({ tweetNo }: { tweetNo: number }) {
+export default async function CommentList({ tweetNo }: CommentListProps) {
   const comments = await findComments(tweetNo);
 
   return (
